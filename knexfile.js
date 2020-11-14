9430669cc5b6a78e9ceffe79926869ebcd19fef0
Rodrigo Tanaka <rodsussumu@gmail.com>
2020-11-13 22:13:31 -0300

    Update connection

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
 
   production: {
     client: 'mysql',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.CLEARDB_DATABASE_URL,
     migrations: {
       tableName: 'knex_migrations'
     }
