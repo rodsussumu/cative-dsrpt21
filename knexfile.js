f70543f754c0a7ee30d0fd41e2c72c13e95db9bf
Rodrigo Tanaka <rodsussumu@gmail.com>
2020-11-13 22:07:41 -0300

    Update connection

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,29 @@
 // Update with your config settings.
 
 module.exports = {
-  client: 'mysql',
-  connection: {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.PORT || 3306,
-    database: process.env.DB_NAME || 'dsrpt21',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || 'root'
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.PORT || 3306,
+      database: process.env.DB_NAME || 'dsrpt21',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || 'root'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
-  pool: {
-    min: 2,
-    max: 10
+
+  production: {
+    client: 'mysql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
 };
--- src/config/db/connection.js
+++ src/config/db/connection.js
@@ -1,6 +1,7 @@
 const knex = require('knex')
+const env = process.env.DB_ENV || 'development'
 const configuration = require('../../../knexfile');
 
-const connection = knex(configuration);
+const connection = knex(configuration[env]);
 
 module.exports = connection;
